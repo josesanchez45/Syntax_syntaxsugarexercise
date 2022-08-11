68c8ec7dad32434da16679d5aa9bcdffa2de835b
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-11 09:56:06 -0700

    first commit

---
+++ Syntax_syntaxsugarexercise/Program.cs
@@ -0,0 +1,16 @@
+﻿//int answer = 4;
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+//Console.WriteLine(response);
+
+var answer = 4;
+var response = (answer < 9) ? "is less than nine" : "is greater than or equal to nine";
+
+Console.WriteLine($"{answer} {response}");
